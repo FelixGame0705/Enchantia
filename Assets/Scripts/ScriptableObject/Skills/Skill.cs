5bdc78c963e2d6dc826119cccfcacf2aa284e247
Tran Tien <tientrangame0705@gmail.com>
2023-12-19 13:52:41 +0700

    Skill manager

---
+++ Assets/Scripts/ScriptableObject/Skills/Skill.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+public abstract class Skill : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public int damage;
+    public float cooldown;
+    public float timeInUsing;//thoi gian tac dung
+    public Sprite icon;
+
+    public virtual void Execute(GameObject player, ref bool isUsingSkill ,ref ATTACK_STAGE stage)
+    {
+    }
+}
